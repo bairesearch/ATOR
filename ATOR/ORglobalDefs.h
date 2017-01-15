e4b3e66d93346548c3affe9ceef66bf8aa305125
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:14:18 +1100

    NO CHANGE RECORD

--- ATOR/ORglobalDefs.h
+++ ATOR/ORglobalDefs.h
@@ -26,7 +26,7 @@
  * File Name: ORglobalDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: OR specific global definitions
- * Project Version: 3d4a 8-June-2014
+ * Project Version: 3d4a 08-June-2014
  * Preconditions: Assume Linux EL5 or Windows XP or is installed
  *
  * 1. Object Recognition Software Installation Instructions;
