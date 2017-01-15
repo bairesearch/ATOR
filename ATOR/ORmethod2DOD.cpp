247a137127916774fe5145edf03e47bac1e123ff
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:14:18 +1100

    NO CHANGE RECORD

--- ATOR/ORmethod2DOD.cpp
+++ ATOR/ORmethod2DOD.cpp
@@ -6,7 +6,8 @@
  * it under the terms of the GNU Affero General Public License version 3
  * only, as published by the Free Software Foundation. The use of
  * intermediary programs or interfaces including file i/o is considered
- * remote network interaction.
+ * remote network interaction. This does not imply such arrangements
+ * do not constitute derivative works.
  *
  * BAIPROJECT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,7 +26,7 @@
  * File Name: ORmethod2DOD.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: ATOR (Axis Transformation Object Recognition) Functions
- * Project Version: 3d3b 24-May-2014
+ * Project Version: 3d4a 8-June-2014
  * NB pointmap is a new addition for test streamlining; NB in test scenarios 2 and 3, there will be no pointmap available; the pointmap will have to be generated after depth map is obtained by using calculatePointUsingTInWorld()
  *******************************************************************************/
 
--- ATOR/ORquadraticFit.cpp
+++ ATOR/ORquadraticFit.cpp
@@ -6,7 +6,8 @@
  * it under the terms of the GNU Affero General Public License version 3
  * only, as published by the Free Software Foundation. The use of
  * intermediary programs or interfaces including file i/o is considered
- * remote network interaction.
+ * remote network interaction. This does not imply such arrangements
+ * do not constitute derivative works.
  *
  * BAIPROJECT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,7 +26,7 @@
  * File Name: ORquadraticFit.cpp (based on EdgiseFrame.java, version 1.17 (26-02-04) CSEM)
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3d3b 24-May-2014
+ * Project Version: 3d4a 8-June-2014
  *
  * Assumes that depth information is less accurate than image information
  *******************************************************************************/
