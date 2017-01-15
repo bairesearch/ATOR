4e4b71e5137288d5fe947ce790e09ebb8f824053
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:18:51 +1100

    NO CHANGE RECORD

--- ATOR/ORTHimageCategorisationNN.h
+++ ATOR/ORTHimageCategorisationNN.h
@@ -26,7 +26,7 @@
  * File Name: ORTHimageCategorisationNN.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: ATOR (Axis Transformation Object Recognition) Functions
- * Project Version: 3f5a 21-July-2015
+ * Project Version: 3f5b 21-July-2015
  * Test Harness for OR Image Categorisation NN method (not yet implemented)
  *******************************************************************************/
 
--- ATOR/ORimagecomparison.cpp
+++ ATOR/ORimagecomparison.cpp
@@ -26,7 +26,7 @@
  * File Name: ORimagecomparison.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: ATOR (Axis Transformation Object Recognition) Functions
- * Project Version: 3f5a 21-July-2015
+ * Project Version: 3f5b 21-July-2015
  *
  *******************************************************************************/
 
