dc750a45f81ab1bac725d42e90d6e6db0c7f5969
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:15:41 +1100

    NO CHANGE RECORD

--- ATOR/ORquadraticFit.cpp
+++ ATOR/ORquadraticFit.cpp
@@ -26,7 +26,7 @@
  * File Name: ORquadraticFit.cpp (based on EdgiseFrame.java, version 1.17 (26-02-04) CSEM)
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3e2c 29-August-2014
+ * Project Version: 3e2d 29-August-2014
  *
  * Assumes that depth information is less accurate than image information
  *******************************************************************************/
