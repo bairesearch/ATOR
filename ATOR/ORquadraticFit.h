9916578f1c91f14f6a86d01f853031e5c57c9fec
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:12:34 +1100

    NO CHANGE RECORD

--- ATOR/ORquadraticFit.h
+++ ATOR/ORquadraticFit.h
@@ -23,7 +23,7 @@
  * File Name: ORquadraticFit.h (based on EdgiseFrame.java, version 1.17 (26-02-04) CSEM)
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Project: Generic Construct Functions
- * Project Version: 3c2a 19-October-2012
+ * Project Version: 3c3a 16-November-2012
  *
  *******************************************************************************/
 
