659a3a906eedc8a333e9fc881548b9e00bbc36d1
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:13:27 +1100

    NO CHANGE RECORD

--- ATOR/ORglobalDefs.cpp
+++ ATOR/ORglobalDefs.cpp
@@ -23,7 +23,7 @@
  * File Name: SHAREDglobalDefs.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Description: Generic Construct Functions: shared global definitions (configure to compile different BAI projects)
- * Project Version: 3c7b 12-October-2013
+ * Project Version: 3c8a 13-October-2013
  *
  *******************************************************************************/
 
--- ATOR/ORglobalDefs.h
+++ ATOR/ORglobalDefs.h
@@ -23,7 +23,7 @@
  * File Name: ORglobalDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Project: OR specific global definitions
- * Project Version: 3c7b 12-October-2013
+ * Project Version: 3c8a 13-October-2013
  * Preconditions: Assume Linux EL5 or Windows XP or is installed
  *
  * 1. Object Recognition Software Installation Instructions;
