739dab484e1959f7a6ab31ea86522b67c40da62a
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:18:44 +1100

    NO CHANGE RECORD

--- ATOR/ORpolygonList.h
+++ ATOR/ORpolygonList.h
@@ -26,7 +26,7 @@
  * File Name: ORpolygonList.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: ATOR (Axis Transformation Object Recognition) Functions
- * Project Version: 3f1a 10-May-2015
+ * Project Version: 3f2b 22-June-2015
  *
  *******************************************************************************/
 
