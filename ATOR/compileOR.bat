1d16a4f08d5bcc6a52376f298fc548dc3627fdb2
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:14:10 +1100

    NO CHANGE RECORD

---
+++ ATOR/compileOR.bat
@@ -0,0 +1,4 @@
+g++ -W --no-warn -I/usr/include/mysql -oOpenOR.exe ORmain.cpp ORmethod.cpp ORmethod3DOD.cpp ORmethod2DOD.cpp LDopengl.cpp ORoperations.cpp ORpixelMaps.cpp ORpolygonList.cpp ORfeature.cpp ORquadraticFit.cpp ORcomparison.cpp ORimagecomparison.cpp ORdatabaseFileIO.cpp ORdatabaseSQL.cpp ORdatabaseDecisionTree.cpp ORrules.cpp ORglobalDefs.cpp RTscene.cpp RToperations.cpp RTparser.cpp RTppm.cpp RTraytracer.cpp RTreferenceManipulation.cpp RTpixelMaps.cpp RTviewinfo.cpp LDparser.cpp LDreferenceManipulation.cpp LDreferenceClass.cpp LDmysql.cpp LDjpeg.cpp SHAREDglobalDefs.cpp SHAREDvector.cpp SHAREDvars.cpp XMLparserClass.cpp XMLrulesClass.cpp -ljpeg -lglut -lGL -lGLU -lmysqlclient
+
+
+
