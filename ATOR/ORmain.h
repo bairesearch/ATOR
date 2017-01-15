8f59d5fd93a010932fabab3f1fcec69e7222fa33
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:14:09 +1100

    NO CHANGE RECORD

--- ATOR/ORmain.h
+++ ATOR/ORmain.h
@@ -23,7 +23,7 @@
  * File Name: ORmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: ATOR (Axis Transformation Object Recognition) Functions
- * Project Version: 3d1a 13-April-2014
+ * Project Version: 3d2a 14-April-2014
  *
  *******************************************************************************/
 #ifndef HEADER_OR_MAIN
