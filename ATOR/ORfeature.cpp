fee235b75d20554ca02458796ae003a20b7c78d8
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:15:48 +1100

    NO CHANGE RECORD

--- ATOR/ORfeature.cpp
+++ ATOR/ORfeature.cpp
@@ -26,7 +26,7 @@
  * File Name: ORfeature.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: ATOR (Axis Transformation Object Recognition) Functions
- * Project Version: 3e5b 03-September-2014
+ * Project Version: 3e6a 07-September-2014
  *
  * Assumes that depth information is less accurate than image information
  *
