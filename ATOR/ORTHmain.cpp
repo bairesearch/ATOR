5a22bac194491764b48271a58a2b59c1bc501ab1
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 15:19:54 +1100

    NO CHANGE RECORD

--- ATOR/ORTHmain.cpp
+++ ATOR/ORTHmain.cpp
@@ -26,7 +26,7 @@
  * File Name: ORTHmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: ATOR (Axis Transformation Object Recognition) Functions
- * Project Version: 3h9b 09-December-2015
+ * Project Version: 3h14a 11-December-2015
  * NB pointmap is a new addition for test streamlining; NB in test scenarios 2 and 3, there will be no pointmap available; the pointmap will have to be generated after depth map is obtained by using calculatePointUsingTInWorld()
  *******************************************************************************/
 
--- ATOR/ORglobalDefs.h
+++ ATOR/ORglobalDefs.h
@@ -26,7 +26,7 @@
  * File Name: ORglobalDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: OR specific global definitions
- * Project Version: 3h9b 09-December-2015
+ * Project Version: 3h14a 11-December-2015
  * Preconditions: Assume Linux EL5 or Windows XP or is installed
  *
  * 1. Object Recognition Software Installation Instructions;
