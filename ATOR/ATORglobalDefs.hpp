1f2a3aea4c7e07be5bee5432b3d7d237f1376a9f
Richard Baxter <richard.baxter@baxterai.com>
2019-09-10 06:06:14 +1000

    NO CHANGE RECORD

--- ATOR/ATORglobalDefs.hpp
+++ ATOR/ATORglobalDefs.hpp
@@ -26,7 +26,7 @@
  * File Name: ATORglobalDefs.hpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: OR specific global definitions
- * Project Version: 3n1a 10-August-2019
+ * Project Version: 3n1b 10-August-2019
  * Preconditions: Assume Linux EL5 or Windows XP or is installed
  * /
  *
